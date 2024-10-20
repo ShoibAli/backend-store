b329a9ef645d9baa68ed1d6393988cb8d7296da4
Shoib Ali <shoibali644@gmail.com>
2024-10-21 00:07:47 +0530

    t

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
 const JWT_SECRET = "alishoib";
 module.exports = { JWT_SECRET };
+
+// {
+//     "version": 2,
+//     "builds": [{ "src": "./index.js", "use": "@vercel/node" }],
+//     "routes": [{ "src": "/(.*)", "dest": "/" }]
+//   }
